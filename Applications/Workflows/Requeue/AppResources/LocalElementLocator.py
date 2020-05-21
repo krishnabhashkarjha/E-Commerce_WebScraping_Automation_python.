7ea4fea6d3712f19eb869c5c74d1b1e7aff5155a
Krishnabhashkar Jha <65655978+krishnabhashkarjha@users.noreply.github.com>
2020-05-21 16:50:57 +0530

    Delete LocalElementLocator.py

--- Applications/Workflows/Requeue/AppResources/LocalElementLocator.py
+++
@@ -1,3 +0,0 @@
-ERROR_HOSPITAL_TAB_ID = 'form1:menuTabs1:7:commandMenuItem1'
-REQUEUE_PARCEL_TAB_XPATH = '//*[@id="form1:panelPage1"]/table[1]/tbody/tr[3]/td/table/tbody/tr[2]/td[5]/span[3]/a'
-REQUEUE_PARCEL_BUTTON_ID = 'form1:commandButton1'
